665285e5384a0a004d1b0741b3c9e836114e650c
t_ohta <phjgt308@ybb.ne.jp>
2010-01-14 09:08:55 +0900

    開発中ソースを一時的に保存

--- length-limited-huffman.lisp
+++ length-limited-huffman.lisp
@@ -59,6 +59,19 @@
               (incf (aref bitlen-table (code-obj-code o)))))
     bitlen-table))
 
+(defun calc-code->bitlength-table.normal-ver (#1=code-frequency-table)
+  (let ((src-objs (make-code-obj-list-from-frequency-table #1#))
+        (bitlen-table (make-array (length #1#) :initial-element 0 :element-type 'fixnum)))
+
+    (loop WITH huffman-trees = src-objs
+          UNTIL (<= (length huffman-trees) 1)
+          FOR (1st 2nd . rest) = huffman-trees 
+      DO
+        (setf huffman-trees (package-and-merge (list 1st 2nd) rest))
+      FINALLY 
+        (let ((huffman-tree (car huffman-trees)))
+          (return (calc-bit-length-from-tree huffman-tree bitlen-table))))))
+
 (defun make-code-obj-list-from-frequency-table (#1=code-frequency-table)
   (sort (loop FOR i FROM 0 BELOW (length #1#)
               WHEN (plusp (aref #1# i))
@@ -78,26 +91,12 @@
    huffman-tree)
   bitlength-table)
 
-(defun calc-code->bitlength-table.normal-ver (#1=code-frequency-table)
-  (let ((src-objs (sort (loop FOR i FROM 0 BELOW (length #1#)
-                              WHEN (plusp (aref #1# i))
-                              COLLECT (make-code-obj :code i :cost (aref #1# i)))
-                        #'< :key #'code-obj-cost))
-        (bitlen-table (make-array (length #1#) :initial-element 0 :element-type 'fixnum)))
 
-    (loop WITH huffman-trees = src-objs
-          UNTIL (<= (length huffman-trees) 1)
-          FOR (1st 2nd . rest) = huffman-trees 
-      DO
-        (setf huffman-trees (package-and-merge (list 1st 2nd) rest))
-      FINALLY 
-        (let ((huffman-tree (car huffman-trees)))
-          (return (calc-bit-length-from-tree huffman-tree bitlen-table))))))
 #|
 [メモ]
 普通のハフマン符号化と長さ制限付きハフマン符号化の計算コストを考えると、
-コードの頻度表と最大ビット長が渡された場合、最大ビット長をオーバーしないようなら、
-普通の(or最適な)ハフマン符号を求めるロジックを使った方が、効率が良さそう。
+(制限付きハフマン符号を求めるために)コードの頻度表と最大ビット長が渡された場合、
+最大ビット長をオーバーしないようなら、普通のハフマン符号を求めるロジックを使った方が、効率が良さそう。
 
 普通のハフマン符号を用いた場合に、指定ビット長をオーバするかどうか(or オーバしそうかどうか)は
 多分、頻度表の各要素を一回走査するだけで求められるような気がする。
