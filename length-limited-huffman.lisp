9b759956c850bf90f7ac1454b041b6a0c3c10f26
t_ohta <phjgt308@ybb.ne.jp>
2010-01-17 16:56:52 +0900

    ヒープ改良前ソース

--- length-limited-huffman.lisp
+++ length-limited-huffman.lisp
@@ -40,6 +40,16 @@
              (traverse-obj fn (cdr (packaged-obj-pair obj)) (1+ depth)))
     (funcall fn obj depth)))
 
+(defun count-each-level-obj (toplevel-objs level-limit)
+  (let ((table (make-array level-limit :initial-element 0)))
+    (dolist (o toplevel-objs)
+      (traverse-obj 
+       (lambda (_ level)
+	 (declare (ignore _))
+	 (incf (aref table level)))
+       o))
+    table))
+
 (defun count-obj (objs &aux (cnt -1))
   (dolist (obj objs)
   (traverse-obj (lambda (_ __) (declare (ignore _ __)) (incf cnt))
@@ -68,6 +78,7 @@
     (loop REPEAT bit-length-limit 
           FOR objs = (package-and-merge objs (copy-list src-objs)) 
           FINALLY
+	  (print (count-each-level-obj (packaging (copy-list objs)) (1+ bit-length-limit)))
           (objs-each (o (packaging objs))
             (incf (aref bitlen-table (code-obj-code o)))))
     bitlen-table))
@@ -83,25 +94,6 @@
     (ceiling (- (log (/ min sum) 2)))))
           
 
-(defun calc-code->bitlength-table.normal-ver (#1=code-frequency-table)
-  (let ((src-objs (make-code-obj-list-from-frequency-table #1#))
-        (bitlen-table (make-array (length #1#) :initial-element 0 :element-type 'fixnum)))
-
-    (loop WITH huffman-trees = src-objs
-          UNTIL (<= (length huffman-trees) 1)
-          FOR (1st 2nd . rest) = huffman-trees 
-      DO
-        (setf huffman-trees (package-and-merge (list 1st 2nd) rest))
-      FINALLY 
-        (let ((huffman-tree (car huffman-trees)))
-          (return (calc-bit-length-from-tree huffman-tree bitlen-table))))))
-
-(defun make-code-obj-list-from-frequency-table (#1=code-frequency-table)
-  (sort (loop FOR i FROM 0 BELOW (length #1#)
-              WHEN (plusp (aref #1# i))
-              COLLECT (make-code-obj :code i :cost (aref #1# i)))
-        #'< :key #'code-obj-cost))
-
 (defun calc-bit-length-from-tree (huffman-tree bitlength-table)
   (traverse-obj  
    (lambda (obj optimal-bit-length &aux (code (code-obj-code obj)))
@@ -110,12 +102,3 @@
   bitlength-table)
 
 
-#|
-[メモ]
-普通のハフマン符号化と長さ制限付きハフマン符号化の計算コストを考えると、
-(制限付きハフマン符号を求めるために)コードの頻度表と最大ビット長が渡された場合、
-最大ビット長をオーバーしないようなら、普通のハフマン符号を求めるロジックを使った方が、効率が良さそう。
-
-普通のハフマン符号を用いた場合に、指定ビット長をオーバするかどうか(or オーバしそうかどうか)は
-多分、頻度表の各要素を一回走査するだけで求められるような気がする。
-|#
